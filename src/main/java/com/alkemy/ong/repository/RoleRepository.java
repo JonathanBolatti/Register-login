40bbcdd64c75905af0cd4996dd54d756a4414a58
Bolatti_Jona <jonathanbolatti@gmail.com>
2022-02-21 19:46:16 -0300

    inicial Commit

---
+++ src/main/java/com/alkemy/ong/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, String> {
+
+}
